c8f5bb9f7a3068304714146c3dbc5743f636f552
Ludmila Alves Pinto <102269120+ludmilaalvespinto@users.noreply.github.com>
2023-07-21 17:37:19 -0300

    adicionando alguns exercícios realizados

---
+++ array_list/aula/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ - métodos de List:
+ Add – Adiciona um elemento ao final da lista
+ AddRange – Adiciona elementos de uma coleção especificada ao final da lista
+ Clear – Remove todos os elementos de uma lista
+Contains – Verifica se um elemento especificado existe ou não em uma lista
+Insert – Insere um elemento em uma posição (índice) especificada na lista
+Remove – Remove a primeira ocorrência de um elemento especificado
+RemoveAt – Remove o elemento em uma posição especificada na lista
+Sort – Ordena os elementos da lista
+
+ */
+
+/*string nome = "Maynara";
+
+for (int cont = nome.Length-1; cont >= 0; cont--)
+{
+    Console.WriteLine(nome[cont]);
+}*/
+
+
+
+/*
+string[] cars = { "fusca", "Volvo", "Jaguar","bmw" };
+
+foreach (var car in cars)
+{
+    Console.Write($"{car}, ");
+}
+*/
+
+
+
+
+
---
+++ matriz/matriz/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string[,] tabuleiro = new string[4, 3];
+int[,] matrizNumeros = { { 11, 22, 33, 44},
+                         { 55, 66, 77,88},
+                         { 01, 12 ,23,24},
+                         {10,20,30,40 }
+};
+
+
+/*for(int i=0; i < matrizNumeros.GetLength(0); i++)
+{
+    for (int j = 0; j < matrizNumeros.GetLength(1); j++)
+    {
+        Console.Write(matrizNumeros[i, j] + "\t");
+    }
+    Console.WriteLine();
+}*/
+
+string[,] tabuleiro = new string[4, 3];
+
+for(int i =0; i < tabuleiro.Length; i++)
+{
+
+}
